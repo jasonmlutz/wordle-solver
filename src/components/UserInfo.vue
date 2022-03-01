d6bf9091a94ce82035af8095c292e173c5c89e94
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-28 19:14:20 -0800

    build and import components

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {ref} from "vue"
+
+const name = ref("");
+const displayName = ref("");
+
+function handleSubmit() {
+  displayName.value = name.value;
+}
+
+</script>
+
+<template> 
+  <div class="UserInfo">
+    <p>{{ displayName }}</p><input
+      v-model="name"
+      placeholder="enter a name"
+    ><button @click="handleSubmit">
+      SUBMIT
+    </button>
+  </div>
+</template>
+
+<style>
+.UserInfo {
+  display: flex;
+  flex-direction: column;
+}
+
+.UserInfo * {
+  width: 100px;
+  margin: 5px auto;
+}
+
+p {
+  width: auto
+}
+</style>
+
