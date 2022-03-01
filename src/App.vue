0e33e955fd251ec92428eb7a97f967a279fd8adc
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-01 10:12:12 -0800

    render letters as test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
-import UserInfoVue from './components/UserInfo.vue';
-import TodoList from './components/TodoList.vue';
+import {ref} from "vue"
+import { v4 as uuid } from 'uuid';
+import alphabet from "./resources/Alphabet"
+
+const letters = ref(alphabet.slice(0,6).map(el => (
+  {letter: el, id: uuid()})))
+let i = 0;
 </script>
 
 <template>
   <div class="container mx-auto bg-gray-500 p-4 border border-white rounded-md flex flex-col items-center">
-    <UserInfoVue />
-    <TodoList />
+    <ul>
+      <li
+        v-for="letter in letters"
+        :key="letter.id"
+      >
+        {{ letter.letter }}
+      </li>
+    </ul>
   </div>
 </template>
