f5d8b707da2cf0fa7fd5f19d7dff41595131f667
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-01 10:36:47 -0800

    Build Letter component

---
+++ src/components/Letter.vue
@@ -0,0 +1,34 @@
+<script setup>
+import alphabet from '../resources/Alphabet';
+
+  const props = defineProps({
+    letter: {
+      type: String,
+      default: "",
+      validator(value) {
+        return value.length === 0 || (value.length === 1 && alphabet.includes(value.toUpperCase(0)))
+      }
+    },
+    id: {
+      type: String,
+      default: `${Date.now()}`
+    },
+    flag: {
+      type: String,
+      default: "absent",
+      validator(value) {
+        return ["absent", "present", "correct"].includes(value)
+      }
+    }
+  })
+</script>
+
+<template>
+  <div class="m-1 bg-black h-[42px] w-[42px] flex flex-row justify-center">
+    <div
+      class="text-3xl"
+    >
+      {{ letter }}
+    </div>
+  </div>
+</template>
