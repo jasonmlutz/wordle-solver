aa4095a1334170720923b7b30d78723df43cf3d5
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-01 12:08:17 -0800

    Improve sytling and implement copies prop

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,52 @@
 <script setup>
-import {reactive} from "vue"
+import {reactive, ref} from "vue"
 import { v4 as uuid } from 'uuid';
 
 import alphabet from "./resources/Alphabet"
 import Letter from "./components/Letter.vue";
 import EmptyRow from "./components/EmptyRow.vue";
 
-const letters = reactive(alphabet.slice(0,6).map(el => (
+const letters = reactive(alphabet.slice(0,5).map(el => (
   {letter: el, id: uuid()})))
+
+const emptyRowCount = ref(5)
 </script>
+
 
 <template>
   <header class="text-xl text-center pt-4">
     Wordle Solver
   </header>
-  <div class="flex flex-col items-center h-4/7 pt-4">
-    <ul class="flex flex-row justify-between w-[300px]">
-      <li
-        v-for="letter in letters"
-        :key="letter.id"
-        class="w-[50px] h-[50px]"
-      >
-        <Letter
+  <div
+    class="flex flex-col items-center pt-4"
+  >
+    <div id="letterGrid">
+      <ul class="flex flex-row justify-between w-[300px]">
+        <li
+          v-for="letter in letters"
           :key="letter.id"
-          :letter="letter.letter"
-        />
-      </li>
-    </ul>
-    <EmptyRow />
-    <EmptyRow />
-    <EmptyRow />
-    <EmptyRow />
-    <EmptyRow />
+          class="w-[60px] h-[60px]"
+        >
+          <Letter
+            :key="letter.id"
+            :letter="letter.letter"
+          />
+        </li>
+      </ul>
+      <EmptyRow :copies="emptyRowCount" />
+    </div>
+    <div
+      id="inputContainer"
+      class="pt-4 flex justify-center w-4/6"
+    >
+      <input
+        type="text"
+        placeholder="5-letter guess"
+        class="text-black px-1 uppercase flex-1"
+      >
+      <button class="bg-white border-2 border-gray-600 rounded-md text-black px-1 uppercase ml-2 hover:bg-gray-400">
+        submit
+      </button>
+    </div>
   </div>
 </template>
