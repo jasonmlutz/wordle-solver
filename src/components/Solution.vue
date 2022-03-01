d169c68d2bf0464d2e2bf80c315493ab8d9ab789
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-01 15:56:33 -0800

    Render solution data from store
    Note a letter + position pair may appear at most once
    in the store, but other overlaps need to be
    addressed when rendered. Fore example, we may learn that
    a letter appears (but not in position 0) and then
    later learn it appears in position 4.
    
    Such a letter should not appear in the incorrect list.
    
    Similarly, a letter which appears in correct or incorrect
    should not appear in absent; e.g. PAPER with
    first letter yellow; the third should not report as absent.

--- src/components/Solution.vue
+++ src/components/Solution.vue
@@ -1,18 +1,51 @@
 <script setup>
-function handleSubmit () {
-  console.log("start search!")
-}
+import { inject, computed } from "vue"
+
+const {store} = inject("store")
+
+// correct
+const correctLetters = computed(() => {
+  const letters = store.value.filter(el => el.flag === 2)
+    .map(el => el.letter)
+  return [...new Set(letters)].sort()
+})
+
+// incorrect
+const incorrectLetters = computed(() => {
+  const letters = store.value.filter(el => el.flag === 1)
+    .map(el => el.letter)
+    .filter(letter => (
+      !correctLetters.value.includes(letter)
+    ))
+  return [...new Set(letters)].sort()
+})
+
+// absent
+const absentLetters = computed(() => {
+  const letters = store.value.filter(el => el.flag === 0)
+    .map(el => el.letter)
+    .filter(letter => (
+      !correctLetters.value.includes(letter) &&
+      !incorrectLetters.value.includes(letter)
+    ))
+  return [...new Set(letters)].sort()
+})
+
+
 </script>
 
 <template>
-  <div
+  <ul
     class="pt-4"
   >
-    <button
-      class="bg-white border-2 border-gray-600 rounded-md text-black px-1 uppercase ml-2 hover:bg-gray-400"
-      @click="handleSubmit"
-    >
-      Find solution
-    </button>
-  </div>
+    <li>
+      Letters not in answer: {{ absentLetters.join(", ") }}
+    </li>
+    <li>
+      Letters in answer, unknown position: {{ incorrectLetters.join(", ") }}
+    </li>
+    <li>
+      Letters in answer, known position: {{ correctLetters.join(", ") }}
+    </li>
+  </ul>
 </template>
