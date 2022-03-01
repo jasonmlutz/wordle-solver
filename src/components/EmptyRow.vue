88924e705e211da412c237cf73129c26fd758bc0
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-01 12:06:44 -0800

    Add copies prop to EmptyRows
    This prop determines the number of empty rows to render,
    so that this component can be called once in App.

--- src/components/EmptyRow.vue
+++ src/components/EmptyRow.vue
@@ -2,6 +2,16 @@
 import {ref} from "vue"
 import Letter from "./Letter.vue"
 
+const props = defineProps({
+  copies: {
+    type: Number,
+    default: 1,
+    validate(value) {
+      return [0, 1, 2, 3, 4, 5, 6].includes(value)
+    }
+  }
+})
+
 let i = 0;
 const emptyLetters = ref([
   {key: i++, letter: " "},
@@ -9,17 +19,26 @@ const emptyLetters = ref([
   {key: i++, letter: " "},
   {key: i++, letter: " "},
   {key: i++, letter: " "},
-  {key: i++, letter: " "}
 ])
+
+const rows = ref(
+  Array.from(Array(props.copies)).map((e, i) => {
+    return {key: i}
+  }
+  ))
 
 </script>
 
 <template>
-  <ul class="flex flex-row justify-between w-[300px]">
+  <ul 
+    v-for="row in rows"
+    :key="row.key"
+    class="flex flex-row justify-between w-[300px]"
+  >
     <li
       v-for="letter in emptyLetters"
       :key="letter.id"
-      class="w-[50px] h-[50px]"
+      class="w-[60px] h-[60px]"
     >
       <Letter
         :letter="letter.letter"
