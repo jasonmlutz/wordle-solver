382a927e6089fd414d3f35380f3ccad1e9e690dd
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-01 10:37:08 -0800

    Implement Letter component and style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import {ref} from "vue"
 import { v4 as uuid } from 'uuid';
+
 import alphabet from "./resources/Alphabet"
+import Letter from "./components/Letter.vue";
 
 const letters = ref(alphabet.slice(0,6).map(el => (
   {letter: el, id: uuid()})))
@@ -10,12 +12,16 @@ let i = 0;
 
 <template>
   <div class="container mx-auto bg-gray-500 p-4 border border-white rounded-md flex flex-col items-center">
-    <ul>
+    <ul class="flex flex-row justify-between w-[300px]">
       <li
         v-for="letter in letters"
         :key="letter.id"
+        class="w-[50px] h-[50px]"
       >
-        {{ letter.letter }}
+        <Letter
+          :key="letter.id"
+          :letter="letter.letter"
+        />
       </li>
     </ul>
   </div>
