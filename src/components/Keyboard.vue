bf51654c53bf258997ff099e0edbe0534a784282
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-03 12:56:41 -0800

    Add coloring to keys depending on board state

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,40 +1,71 @@
 <script setup>
 
-import {ref} from "vue"
+import {inject, watch, ref} from "vue"
 
 const props = defineProps({
   deleteLastLetter: {
     type: Function,
+    default: null,
   },
   handleSubmit: {
     type: Function,
+    default: null,
   },
   toggleKeyboard: {
     type: Function,
+    default: null,
   },
   clearAllLetters: {
     type: Function,
+    default: null,
   }
 })
 
-var firstRow = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
-firstRow = firstRow.map((el, index) => {
-  return {letter: el, index: index}
+const buttonStyle = "text-s border rounded-md bg-gray-600 m-0.5 w-12 h-14 text-center hover:bg-gray-700"
+
+var letterStyle = "uppercase border rounded-md m-0.5 w-8 h-14 pt-3 text-xl text-center hover:bg-gray-700"
+
+var keyboardLetters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"] // length 10
+keyboardLetters = keyboardLetters.concat(["a", "s", "d", "f", "g", "h", "j", "k", "l"]) //9 more
+keyboardLetters = keyboardLetters.concat(["z", "x", "c", "v", "b", "n", "m"]) // 7 more
+
+const keyboardKeys = ref(keyboardLetters.map((el, index) => {
+  return {letter: el, index: index, color: " bg-gray-600"}
+}))
+
+function handleClear() {
+  keyboardKeys.value = keyboardKeys.value.map(key => {
+    return {...key, color: " bg-gray-600"}
+  })
+  props.clearAllLetters()
+}
+
+const { store } = inject("store");
+
+watch(store.value, () => {
+  keyboardKeys.value = keyboardKeys.value.map(key => {
+    var reversedStore = [...store.value].reverse()
+    var letterData = reversedStore.find(data => data.letter.toUpperCase() === key.letter.toUpperCase())
+    letterData = letterData || {}
+    var letterColor
+    switch (letterData.flag) {
+    case 0:
+      letterColor = " bg-gray-600"
+      break;
+    case 1:
+      letterColor = " bg-yellow-500"
+      break;
+    case 2:
+      letterColor = " bg-green-800"
+      break;
+    default:
+      letterColor = " bg-gray-600"
+      break;
+    }
+
+    return {...key, color: letterColor}
+  })
 })
-
-var secondRow = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
-secondRow = secondRow.map((el, index) => {
-  return {letter: el, index: index}
-})
-
-var thirdRow = ["z", "x", "c", "v", "b", "n", "m"]
-thirdRow = thirdRow.map((el, index) => {
-  return {letter: el, index: index}
-})
-
-const letterStyle = ref("uppercase border rounded-md bg-gray-500 m-0.5 w-8 h-14 pt-3 text-xl text-center hover:bg-gray-700")
-const buttonStyle = ref("text-s border rounded-md bg-gray-500 m-0.5 w-12 h-14 pt-4 text-center hover:bg-gray-700")
-
 </script>
 
 <template>
@@ -47,9 +78,9 @@ const buttonStyle = ref("text-s border rounded-md bg-gray-500 m-0.5 w-12 h-14 pt
       class="flex flex-row justify-between"
     >
       <li
-        v-for="element in firstRow"
+        v-for="element in keyboardKeys.slice(0,10)"
         :key="element.index"
-        :class="letterStyle"
+        :class="letterStyle + element.color"
         @click="handleSubmit(element.letter)"
       >
         {{ element.letter }}
@@ -60,9 +91,9 @@ const buttonStyle = ref("text-s border rounded-md bg-gray-500 m-0.5 w-12 h-14 pt
       class="flex flex-row justify-center"
     >
       <li
-        v-for="element in secondRow"
+        v-for="element in keyboardKeys.slice(10,19)"
         :key="element.index"
-        :class="letterStyle"
+        :class="letterStyle + element.color"
         @click="handleSubmit(element.letter)"
       >
         {{ element.letter }}
@@ -73,21 +104,21 @@ const buttonStyle = ref("text-s border rounded-md bg-gray-500 m-0.5 w-12 h-14 pt
       class="flex flex-row justify-center"
     >
       <li
-        :class="buttonStyle + ` text-xs`"
-        @click="clearAllLetters"
+        :class="buttonStyle + ` text-xs  pt-5`"
+        @click="handleClear"
       >
         CLEAR
       </li>
       <li
-        v-for="element in thirdRow"
+        v-for="element in keyboardKeys.slice(19, 26)"
         :key="element.index"
-        :class="letterStyle"
+        :class="letterStyle + element.color"
         @click="handleSubmit(element.letter)"
       >
         {{ element.letter }}
       </li>
       <li
-        :class="buttonStyle"
+        :class="buttonStyle + ` pt-4`"
         @click="deleteLastLetter"
       >
         DEL
